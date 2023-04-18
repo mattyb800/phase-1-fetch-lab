2a08bbe43d21816d4f3de6c72b5d28acff34f1be
Matt Brouwer <notjonbonjovi@gmail.com>
2023-04-17 20:01:21 -0400

    fetch

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function fetchBooks() {
+ return fetch('https://anapioficeandfire.com/api/books')
+  .then(res => res.json())
+  .then((object) => renderBooks(object))
   // To pass the tests, don't forget to return your fetch!
   
 }
